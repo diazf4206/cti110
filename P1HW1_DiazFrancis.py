0f9da0786194dbe91b3e20ae393c2cd8d76c00f6
diazf4206 <160438140+diazf4206@users.noreply.github.com>
2024-02-25 14:32:17 -0500

    Add files via upload
    First entry for P1HW1

---
+++ P1HW1_DiazFrancis.py
@@ -0,0 +1,15 @@
+#Francis Diaz
+#02/25/2024
+#P1HW1
+#For this assignment, refer to section LAB: Warm up: Basic output with variables in your ZyBook.Using Python's input and print functions, we will create a program.
+
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num **2)
+print('And', user_num, 'cubed is', user_num **3)
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num, '+', user_num2, 'is', user_num + user_num2)
+print(user_num, '*', user_num2, 'is', user_num * user_num2)
